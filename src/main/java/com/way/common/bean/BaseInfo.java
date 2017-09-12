e11ab9a30ddd0203d18de0501747c69f92e38433
xuxinpei <649087712@qq.com>
2017-09-12 13:58:09 +0800

    优化

---
+++ src/main/java/com/way/common/bean/BaseInfo.java
@@ -0,0 +1,14 @@
+package com.way.common.bean;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @ClassName: BaseInfo
+ * @Description: 基础的序列化对象
+ * @author: xinpei.xu
+ * @date: 2017/08/14 21:45
+ *
+ */
+public class BaseInfo implements Serializable {
+}
